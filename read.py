00efd9e4bd308c94f1ee2721ae4f2c43baab450b
Samuel Yang <samuel0626@gmail.com>
2020-10-11 15:44:40 +0800

    add filter function

--- read.py
+++ read.py
@@ -16,3 +16,12 @@ for d in data:
 	sum_len = sum_len + len(d)
 
 print(sum_len / len(data))
+
+new = []
+
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆資料長度小於100')
+print(new[0])
+print(new[1])
